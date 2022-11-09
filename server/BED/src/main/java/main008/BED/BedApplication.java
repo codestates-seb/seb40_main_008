e5cd5c654c507ed846b4b5f5c2e049d096faa972
hyojin <hyoreal51@gmail.com>
2022-11-09 11:28:11 +0900

    BE: [Feat] add spring server

---
+++ server/BED/src/main/java/main008/BED/BedApplication.java
@@ -0,0 +1,13 @@
+package main008.BED;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BedApplication.class, args);
+	}
+
+}
---
+++ server/BED/src/test/java/main008/BED/BedApplicationTests.java
@@ -0,0 +1,13 @@
+package main008.BED;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
