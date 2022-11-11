995bbec1702c438fc4b9ba349e4566f0c990fecb
Mingi Jeon <103816228+podoco@users.noreply.github.com>
2022-11-11 14:58:21 +0900

    navbar (#13)
    create navbar

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,37 +1,37 @@
-import React from 'react';
-import QuestionCard from '../components/Card/QuestionCard';
-import Carousel from '../components/Carousel/Carousel';
-import { ICarousel } from '../types/carousel';
+import React from "react";
+import QuestionCard from "../components/Card/QuestionCard";
+import Carousel from "../components/Carousel/Carousel";
+import { ICarousel } from "../types/carousel";
 
 const getGoogleSession = async () => {};
 
 const getQuestionList = async () => {
-	const response = await fetch('https://pioneroroom.com/questionlist');
-	const data = await response.json();
-	return data;
+  const response = await fetch("https://pioneroroom.com/questionlist");
+  const data = await response.json();
+  return data;
 };
 
 const getCarouselImages = async () => {
-	const response = await fetch(
-		'https://rajmdhzzh0zidy3bkmrny2liadj1.requestly.me/AmateurAmaranthThrush'
-	);
-	const json = await response.json();
-	const imageArr: ICarousel[] = json.data;
-	return imageArr;
+  const response = await fetch(
+    "https://rajmdhzzh0zidy3bkmrny2liadj1.requestly.me/AmateurAmaranthThrush"
+  );
+  const json = await response.json();
+  const imageArr: ICarousel[] = json.data;
+  return imageArr;
 };
 
 const page = async ({ Question }: any) => {
-	// const imageArr = await getCarouselImages();
-	const data = await getQuestionList();
+  // const imageArr = await getCarouselImages();
+  const data = await getQuestionList();
 
-	return (
-		<div>
-			{/* <Carousel carousel={imageArr} /> */}
-			{data.data.map((e: any) => {
-				return <QuestionCard key={e.questionId} question={e} />;
-			})}
-		</div>
-	);
+  return (
+    <div>
+      {/* <Carousel carousel={imageArr} /> */}
+      {data.data.map((e: any) => {
+        return <QuestionCard key={e.questionId} question={e} />;
+      })}
+    </div>
+  );
 };
 
 export default page;
