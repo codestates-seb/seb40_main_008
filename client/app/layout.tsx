23676989d8664450bf08814cc567c272e6945b13
Mingi Jeon <103816228+podoco@users.noreply.github.com>
2022-11-14 17:40:42 +0900

    FE:[Function] Homenavbar according to scroll (#15)
    * scroll
    
    * sticky navbar
    
    * modified navbar

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,38 +1,40 @@
-import { Noto_Sans_KR } from '@next/font/google';
-import NavBar from '../components/Navbar/NavBar';
-import SessionContainer from '../components/Providers/SessionProvider';
-import '../styles/globals.css';
-import { unstable_getServerSession } from 'next-auth';
-import { getSession } from '../utils/helper/session';
-import { cookies, headers } from 'next/headers';
+import { Roboto, Noto_Sans_KR } from "@next/font/google";
+import NavBar from "../components/HomeNavBar/HomeNavBar";
+import "../styles/globals.css";
+
+import SessionContainer from "../components/Providers/SessionProvider";
+import "../styles/globals.css";
+import { unstable_getServerSession } from "next-auth";
+import { getSession } from "../utils/helper/session";
+import { cookies, headers } from "next/headers";
 
 const noto = Noto_Sans_KR({
-	weight: '400',
-	fallback: ['Roboto'],
-	subsets: ['latin'],
+  weight: "400",
+  fallback: ["Roboto"],
+  subsets: ["latin"],
 });
 
 const RootLayout = async ({ children }: any) => {
-	const { segment } = children.props.childProp;
-	const session = await getSession(headers().get('cookie') ?? '');
-	const nextCookies = cookies();
-	return (
-		<html className={noto.className}>
-			<head>
-				<meta name="viewport" content="width=device-width,initial-scale=1" />
-				<title>asdf</title>
-			</head>
-			<body>
-				<SessionContainer session={session}>
-					<div className="main">
-						{/* @ts-expect-error Server Component */}
-						{segment !== 'login' ? <NavBar /> : null}
-						{children}
-					</div>
-				</SessionContainer>
-			</body>
-		</html>
-	);
+  const { segment } = children.props.childProp;
+  const session = await getSession(headers().get("cookie") ?? "");
+  const nextCookies = cookies();
+  return (
+    <html className={noto.className}>
+      <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>asdf</title>
+      </head>
+      <body>
+        <SessionContainer session={session}>
+          <div className="main">
+            {/* @ts-expect-error Server Component */}
+            {segment !== "login" ? <NavBar /> : null}
+            {children}
+          </div>
+        </SessionContainer>
+      </body>
+    </html>
+  );
 };
 
 export default RootLayout;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,26 +1,30 @@
-import Link from 'next/link';
-import React from 'react';
-import QuestionCard from '../components/Card/QuestionCard';
+import React from "react";
+import QuestionCard from "../components/Card/QuestionCard";
+import Carousel from "../components/Carousel/Carousel";
+import HomeNavBar from "../components/HomeNavBar/HomeNavBar";
+import { ICarousel } from "../types/carousel";
+
+const getGoogleSession = async () => {};
 
 const getQuestionList = async () => {
-	const response = await fetch(`https://pioneroroom.com/questionlist`);
-	const data = await response.json();
-	return data;
+  const response = await fetch(`https://pioneroroom.com/questionlist`);
+  const data = await response.json();
+  return data;
 };
 
 const page = async ({ Question }: any) => {
-	// const imageArr = await getCarouselImages();
-	const data = await getQuestionList();
+  // const imageArr = await getCarouselImages();
+  const data = await getQuestionList();
 
-	return (
-		<div>
-			<Link href={'/login'}>Login page</Link>
-			{/* <Carousel carousel={imageArr} /> */}
-			{data.data.map((e: any) => {
-				return <QuestionCard key={e.questionId} question={e} />;
-			})}
-		</div>
-	);
+  return (
+    <div>
+      <HomeNavBar />
+      {/* <Carousel carousel={imageArr} /> */}
+      {data.data.map((e: any) => {
+        return <QuestionCard key={e.questionId} question={e} />;
+      })}
+    </div>
+  );
 };
 
 export default page;
