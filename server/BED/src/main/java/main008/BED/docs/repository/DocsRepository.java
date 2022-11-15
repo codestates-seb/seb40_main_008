f15e198f52abb54de1de959fd9bb508bc66af1af
Seung-IL-Bang <seungilbang@khu.ac.kr>
2022-11-15 16:52:08 +0900

    BE: [Feat] Create, Read of Docs (#27)
    * [ADD] be/seungil branch
    
    * [FIX] fix to restart
    
    * [ADD] new structure
    
    * [UPD] structure
    
    * [Feat] create, read of docs

--- server/BED/src/main/java/main008/BED/docs/repository/DocsRepository.java
+++ server/BED/src/main/java/main008/BED/docs/repository/DocsRepository.java
@@ -2,8 +2,16 @@ package main008.BED.docs.repository;
 
 import main008.BED.docs.entity.Docs;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DocsRepository extends JpaRepository<Docs, Long> {
+
+//    Docs findByName(String name);
+
+    boolean existsByName(String name);
+
+    boolean existsById(Long id);
+
 }
--- server/BED/src/main/java/main008/BED/uploadClass/dto/UploadClassDto.java
+++ server/BED/src/main/java/main008/BED/uploadClass/dto/UploadClassDto.java
@@ -2,12 +2,15 @@ package main008.BED.uploadClass.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
 
 public class UploadClassDto {
 
     @Getter
     @AllArgsConstructor
     public static class Post {
+        private String title;
+        private MultipartFile video;
 
     }
 
