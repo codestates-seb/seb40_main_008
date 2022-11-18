a0d7f5668a2a8bb8fae6589813d4c999de0ced40
podoco <103816228+podoco@users.noreply.github.com>
2022-11-18 12:21:30 +0900

    캐러셀 title추가

--- client/app/HomeCarouselSection.tsx
+++ client/app/HomeCarouselSection.tsx
@@ -1,13 +1,169 @@
 "use client";
-import React from "react";
+import React, {
+  useMemo,
+  Children,
+  useLayoutEffect,
+  useState,
+  useRef,
+  useEffect,
+  useCallback,
+} from "react";
 import { CarouselInfo } from "../types/homeScreen/carousel";
+import styles from "./styles/HomeCarouselSection.module.css";
 
 interface HomeCarouselSectionProps {
+  children: any;
   carouselList: Array<CarouselInfo>;
 }
 
-const HomeCarouselSection = ({ carouselList }: HomeCarouselSectionProps) => {
-  return <div>{JSON.stringify(carouselList)}</div>;
+const HomeCarouselSection = ({
+  children,
+  carouselList,
+}: HomeCarouselSectionProps) => {
+  console.log(children);
+  const containerRef = useRef<HTMLUListElement>(null);
+  const intervalRef = useRef<MutableRefObject>(null);
+  const [current, setCurrent] = useState(1);
+  const [translateX, setTranslateX] = useState(0);
+
+  const actionHandler = useCallback(
+    (mode) => {
+      if (containerRef.current) {
+        containerRef.current.style.transitionDuration = "1000ms";
+      }
+      if (mode === "prev") {
+        if (current <= 1) {
+          setTranslateX(0);
+          setCurrent(children.length);
+        } else {
+          if (containerRef.current) {
+            setTranslateX(containerRef.current.clientWidth * (current - 1));
+            setCurrent((prev) => --prev);
+          }
+        }
+      } else if (mode === "next") {
+        if (current >= children.length) {
+          if (containerRef.current) {
+            setTranslateX(
+              containerRef.current.clientWidth * (children.length + 1)
+            );
+            setCurrent(1);
+          }
+        } else {
+          if (containerRef.current) {
+            setTranslateX(containerRef.current.clientWidth * (current + 1));
+            setCurrent((prev) => ++prev);
+          }
+        }
+      }
+    },
+    [current, children]
+  );
+
+  //this is for infinite scroll smooth effect
+  useEffect(() => {
+    const transitionEnd = () => {
+      if (current <= 1 && containerRef.current) {
+        containerRef.current.style.transitionDuration = "0ms";
+        setTranslateX(containerRef.current.clientWidth * current);
+      }
+      if (current >= children.length) {
+        if (containerRef.current) {
+          containerRef.current.style.transitionDuration = "0ms";
+          setTranslateX(containerRef.current.clientWidth * children.length);
+        }
+      }
+    };
+    document.addEventListener("transitionend", transitionEnd);
+
+    return () => {
+      document.removeEventListener("transitionend", transitionEnd);
+    };
+  }, [current, children]);
+
+  //for autoplay
+  useEffect(() => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+    intervalRef.current = setInterval(() => {
+      actionHandler("next");
+    }, 3000);
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, [actionHandler]);
+
+  const slides = useMemo(() => {
+    if (children.length > 1) {
+      let items = children.map((child: any, index: number) => (
+        <li key={index} className={styles.Slide}>
+          {child}
+        </li>
+      ));
+
+      return [
+        <li key={children.length + 1} className={styles.Slide}>
+          {children[children.length - 1]}
+        </li>,
+        ...items,
+        <li key={children.length + 2} className={styles.Slide}>
+          {children[0]}
+        </li>,
+      ];
+    }
+    return <li className={styles.Slide}>{children[0]}</li>;
+    //return null;
+  }, [children]);
+
+  useLayoutEffect(() => {
+    if (containerRef.current) {
+      setTranslateX(containerRef.current.clientWidth * current);
+    }
+  }, []);
+
+  return (
+    <section className={styles.carouselSection}>
+      <ul
+        ref={containerRef}
+        className={styles.Container}
+        style={{
+          transform: `translate3d(${-translateX}px,0,0)`,
+        }}
+      >
+        {slides}
+      </ul>
+      <div className={styles.titlecontainer}>
+        asdas
+        {carouselList &&
+          carouselList.map((e) => (
+            <>
+              <p className={styles.title} key={e.id}>
+                {e.title}dsa
+              </p>
+              <p className={styles.subtitile} key={e.id}>
+                {e.subtitle}
+              </p>
+            </>
+          ))}
+      </div>
+
+      <button
+        onClick={() => actionHandler("prev")}
+        className={`${styles.Btn} ${styles.BtnLeft}`}
+      >
+        {"<"}
+      </button>
+      <button
+        onClick={() => actionHandler("next")}
+        className={`${styles.Btn} ${styles.BtnRight}`}
+      >
+        {">"}
+      </button>
+    </section>
+  );
 };
 
 export default HomeCarouselSection;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,6 +4,12 @@ import { CarouselInfo } from "../types/homeScreen/carousel";
 import { Content } from "../types/homeScreen/mainVideoContents";
 import HomeCarouselSection from "./HomeCarouselSection";
 import HomeClassesSection from "./HomeClassesSection";
+import styles from "./styles/page.module.css";
+import Image from "next/image";
+import HomeCarouselTitle from "./HomeCarouselTitle";
+import { Erica_One } from "@next/font/google";
+import { relative } from "path";
+import { HomeCarouselList } from "../components/Carousel/HomeCarouselList";
 
 const getClassesContents = async (): Promise<Array<Content>> => {
   try {
@@ -29,6 +35,7 @@ const getCarouselInfo = async (): Promise<Array<CarouselInfo>> => {
       "https://run.mocky.io/v3/dd10a02d-8e88-483a-a270-c284b4d4e81d"
     );
     const { carouselInfo } = await response.json();
+    console.log("C", carouselInfo.length);
     return carouselInfo;
   } catch (error) {
     alert(error);
@@ -41,10 +48,43 @@ const page = async () => {
   const carouselList = await getCarouselInfo();
   return (
     <>
-      <HomeNavBar />
-      <HomeCarouselSection carouselList={carouselList} />
-      <HomeClassesSection contentList={contentsList} />
-      <TabNavigator activeLink={"home"} />
+      <div className={styles.contentbody}>
+        <HomeNavBar />
+        <div className={styles.carousel}>
+          <HomeCarouselSection caroselList={carouselList}>
+            {carouselList.map((e) => (
+              <div
+                key={e.id}
+                style={{ position: "relative", border: "1px solid orange" }}
+              >
+                <Image
+                  key={e.id}
+                  src={e.src}
+                  alt="img"
+                  //fill = true 를 쓰려면 상위 div가 relative
+                  width={412}
+                  height={462}
+                  style={{ objectFit: "cover" }}
+                />
+                <div
+                  style={{
+                    height: "200px",
+                    width: "200px",
+                    border: "1px solid red",
+                    position: "absolute",
+                    zIndex: 1000,
+                    bottom: 0,
+                  }}
+                >
+                  <h2 style={{ color: "black" }}>{e.title}</h2>
+                </div>
+              </div>
+            ))}
+          </HomeCarouselSection>
+        </div>
+        <HomeClassesSection contentList={contentsList} />
+        <TabNavigator activeLink={"home"} />
+      </div>
     </>
   );
 };
