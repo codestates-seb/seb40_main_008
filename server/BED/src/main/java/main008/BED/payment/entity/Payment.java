348ed04a304227b8dae857c1ed6096823d9e004f
hyojin <hyoreal51@gmail.com>
2022-11-18 14:25:36 +0900

    BE: [Feat] add Payment Entity

---
+++ server/BED/src/main/java/main008/BED/payment/entity/Payment.java
@@ -0,0 +1,37 @@
+package main008.BED.payment.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import main008.BED.contents.entity.Contents;
+import main008.BED.users.entity.Users;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+
+    @Column
+    private Boolean payed;
+
+    @Column
+    private ZonedDateTime boughtAt;
+
+    @OneToOne
+    @JoinColumn(name = "USERS_ID")
+    private Users users;
+
+    @OneToOne
+    @JoinColumn(name = "CONTENTS_ID")
+    private Contents contents;
+}
