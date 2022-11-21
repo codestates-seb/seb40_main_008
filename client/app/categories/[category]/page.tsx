cb47220a3fa26140f30ca122e8c3c5240d91dc00
wongee93 <jhwon9304@gmail.com>
2022-11-21 21:46:42 +0900

    add category links and class price

---
+++ client/app/categories/[category]/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const fetchGet = async (category: any) => {
+    const res = await fetch(`https://pioneroroom.com/contentsList/${category}`)
+    return await res.json().then((res) => res.data);
+}
+
+const DetailCategoryPage = async ({ params }: any) => {
+    const get = await fetchGet(params.categoryId);
+    return (
+        <div>
+            <p>{JSON.stringify(get)}</p>
+        </div>
+    )
+}
+
+export default DetailCategoryPage;
+
+// 카테고리 필터링 작업
+
+// 1. categoryId별로 필터링된 api에서 가져오면 될지?(카테고리/최신순/인기순)
+// 2. 전체 카테고리 리스트를 주시면 프론트에서 필터링..?
+// 3. 카테고리 네이밍 BE에서 정한게 있는지?
+// --> 백엔드에 쿼리스트링으로 넘겨주기(스티랑 값/URL 받으면 이어 작업하기)
--- client/app/categories/page.tsx
+++ client/app/categories/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import BaseNavbar from '../../components/BaseNavBar/BaseNavbar'
 import styles from './categories.module.css'
+import Link from "next/link";
 
 const Categories = () => {
     return (
@@ -8,17 +9,17 @@ const Categories = () => {
             <BaseNavbar />
             <div className={styles.categorytop}>
                 <span className={styles.categoryleft}>
-                    <span className={styles.link}>디지털 드로잉</span>
-                    <span className={styles.link}>드로잉</span>
-                    <span className={styles.link}>공예</span>
-                    <span className={styles.link}>요리/음료</span>
-                    <span className={styles.link}>베이킹/디저트</span>
+                    <span className={styles.link}><Link href={"/categories/digitaldrawing"}>디지털 드로잉</Link></span>
+                    <span className={styles.link}><Link href={"/categories/drawing"}>드로잉</Link></span>
+                    <span className={styles.link}><Link href={"/categories/crafts"}>공예</Link></span>
+                    <span className={styles.link}><Link href={"/categories/food_beverage"}>요리/음료</Link></span>
+                    <span className={styles.link}><Link href={"/categories/baking_dessert"}>베이킹/디저트</Link></span>
                 </span>
                 <span className={styles.categoryright}>
-                    <span className={styles.link}>음악</span>
-                    <span className={styles.link}>운동</span>
-                    <span className={styles.link}>라이프스타일</span>
-                    <span className={styles.link}>사진/영상</span>
+                    <span className={styles.link}><Link href={"/categories/music"}>음악</Link></span>
+                    <span className={styles.link}><Link href={"/categories/exercise"}>운동</Link></span>
+                    <span className={styles.link}><Link href={"/categories/lifestyle"}>라이프스타일</Link></span>
+                    <span className={styles.link}><Link href={"/categories/photo_video"}>사진/영상</Link></span>
                 </span>
             </div>
 
@@ -26,11 +27,11 @@ const Categories = () => {
 
             <div className={styles.category}>
                 <span className={styles.categoryleft}>
-                    <span className={styles.link}>금융/재테크</span>
-                    <span className={styles.link}>창업/부업</span>
+                    <span className={styles.link}><Link href={"/categories/finance"}>금융/재테크</Link></span>
+                    <span className={styles.link}><Link href={"/categories/startup"}>창업/부업</Link></span>
                 </span>
                 <span className={styles.categoryright}>
-                    <span className={styles.link}>성공마인드</span>
+                    <span className={styles.link}><Link href={"/categories/mind"}>성공마인드</Link></span>
                 </span>
             </div>
 
@@ -38,16 +39,16 @@ const Categories = () => {
 
             <div className={styles.category}>
                 <span className={styles.categoryleft}>
-                    <span className={styles.link}>프로그래밍</span>
-                    <span className={styles.link}>데이터사이언스</span>
-                    <span className={styles.link}>제품 기획</span>
-                    <span className={styles.link}>비즈니스</span>
+                    <span className={styles.link}><Link href={"/categories/programming"}>프로그래밍</Link></span>
+                    <span className={styles.link}><Link href={"/categories/datascience"}>데이터사이언스</Link></span>
+                    <span className={styles.link}><Link href={"/categories/product_planning"}>제품 기획</Link></span>
+                    <span className={styles.link}><Link href={"/categories/business"}>비즈니스</Link></span>
                 </span>
                 <span className={styles.categoryright}>
-                    <span className={styles.link}>생산성</span>
-                    <span className={styles.link}>마케팅</span>
-                    <span className={styles.link}>디자인</span>
-                    <span className={styles.link}>영상/3D</span>
+                    <span className={styles.link}><Link href={"/categories/productivity"}>생산성</Link></span>
+                    <span className={styles.link}><Link href={"/categories/marketing"}>마케팅</Link></span>
+                    <span className={styles.link}><Link href={"/categories/design"}>디자인</Link></span>
+                    <span className={styles.link}><Link href={"/categories/video_3d"}>영상/3D</Link></span>
                 </span>
             </div>
 
@@ -55,11 +56,11 @@ const Categories = () => {
 
             <div className={styles.category}>
                 <span className={styles.categoryleft}>
-                    <span className={styles.link}>영어</span>
-                    <span className={styles.link}>제2 외국어</span>
+                    <span className={styles.link}><Link href={"/categories/english"}>영어</Link></span>
+                    <span className={styles.link}><Link href={"/categories/second_language"}>제2 외국어</Link></span>
                 </span>
                 <span className={styles.categoryright}>
-                    <span className={styles.link}>외국어 시험</span>
+                    <span className={styles.link}><Link href={"/categories/language_test"}>외국어 시험</Link></span>
                 </span>
             </div>
 
@@ -67,10 +68,10 @@ const Categories = () => {
 
             <div className={styles.category}>
                 <span className={styles.categoryleft}>
-                    <span className={styles.link}>아이 교육</span>
+                    <span className={styles.link}><Link href={"/categories/child_edu"}>아이 교육</Link></span>
                 </span>
                 <span className={styles.categoryright}>
-                    <span className={styles.link}>부모 교육</span>
+                    <span className={styles.link}><Link href={"/categories/parents_edu"}>부모 교육</Link></span>
                 </span>
             </div>
 
