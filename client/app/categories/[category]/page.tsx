39ba8eed7aa64b511efb27ac50da357a1126f1e7
wongee93 <jhwon9304@gmail.com>
2022-11-24 17:38:58 +0900

    add categorypage

--- client/app/categories/[category]/page.tsx
+++ client/app/categories/[category]/page.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
 import { ICategorySearchResult } from '../../../types/category_search/categorySearchType';
 import SearchResultFilter from '../../../components/Search/SearchResultFilter';
+import HomeClassesSection from '../../../components/Card/HomeClassesSection';
+import { Content } from '../../../types/homeScreen/mainVideoContents';
 
 // geneticStaticParams 도 만들기,,,(detail,,,에 있는 것과 같이)
 // Homeclasses Section interface type도 ICategorySearchResult로 바꾸기
 // 필터링 부분 컴포넌트화 하기
 // Classes 그리드 부분 맞춰서 가져다 쓰기
 
+const getClassesContents = async (): Promise<Array<Content>> => {
+    try {
+        const response = await fetch('https://run.mocky.io/v3/3990c908-5af6-4850-9501-fa41adb80109', {
+            next: {
+                revalidate: 60,
+            },
+        });
+        const { contentsList } = await response.json();
+        return contentsList;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+};
+
+interface HomeContentProps {
+    contentList: Content[];
+}
+
 const fetchCategoryData = async (
     categoryName: string
 ): Promise<ICategorySearchResult[]> => {
-    // const res = await fetch(https://pioneroroom.com/contentsList/${category});
     const res = await fetch(
         `https://pioneroroom.com/category/sort/likes?categories=${categoryName}`
     );
@@ -20,6 +40,7 @@ const fetchCategoryData = async (
 
 const DetailCategoryPage = async ({ params }: any) => {
 
+    const contentsList = await getClassesContents();
     const get = await fetchCategoryData(params.categoryName);
 
     return (
@@ -27,11 +48,13 @@ const DetailCategoryPage = async ({ params }: any) => {
             <SearchResultFilter searchTerm={params.categoryName} />
             <div>
 
+                {/* <p>카테고리 이름{params.categoryName}</p> */}
+                {/* <p>json 정보{JSON.stringify(get)}</p> */}
+
+                <HomeClassesSection contentList={contentsList} />
 
                 {/* 갖가지 강의영상에 대한 정보 그리드 정보 */}
 
-
-                {/* <p>{JSON.stringify(get)}</p> */}
             </div>
         </>
     );
--- client/components/Search/SearchResultFilter.tsx
+++ client/components/Search/SearchResultFilter.tsx
@@ -1,24 +1,25 @@
+'use client'
 import React from 'react'
+import { redirect } from 'next/navigation';
 import styles from './SearchResultFilter.module.css'
 
 const SearchResultFilter = ({ params }: any) => {
 
-    // const handleSortChange = (e: any) => {
-    //     if (현재필터링조건 === e.target.value) return;
-    //     redirect(/category/sort / { e.target.value } / categoies={ searchTerm })
-    // }
+    const handleSortChange = (e: any) => {
+        if (params.categoryName === e.target.value) return;
+
+        redirect(`/category/sort/${e.target.value}?categoies=${params.categoryName}`)
+        //http://localhost:8080/category/sort/newest?categories=PROGRAMMING
+        //http://localhost:8080/category/sort/likes?categories=PROGRAMMING
+
+    }
 
     return (
 
-        // 필터링에 관한 정보. 
-        // <div>
-        // <input onChange={handleSortChange} />
-        // </div>
-
         <div className={styles.filterWrapper}>
-            <select className={styles.selectzone}>
-                <option className={styles.options}>인기순</option>
-                <option className={styles.options}>최신순</option>
+            <select className={styles.selectzone} onChange={handleSortChange}>
+                <option value='likes' className={styles.options}>인기순</option>
+                <option value='newest' className={styles.options}>최신순</option>
             </select>
         </div>
     )
