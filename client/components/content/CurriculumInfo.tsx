c7d42a9ea7b5dd9847329c9f1edb5b16bc4a6380
podoco <103816228+podoco@users.noreply.github.com>
2022-11-25 13:49:52 +0900

    커리큘럼tab 디자인

--- client/components/content/CurriculumInfo.tsx
+++ client/components/content/CurriculumInfo.tsx
@@ -1,7 +1,43 @@
 import React from "react";
 import styles from "./CurriculumInfo.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencil } from "@fortawesome/free-solid-svg-icons";
 const CurriculumInfo = () => {
-  return <div>CurriculumInfo</div>;
+  return (
+    <div className={styles.Wrapper}>
+      <div className={styles.thumbnail}>챕터썸네일</div>
+      <div className={styles.chapterWrapper}>
+        <div className={styles.chapterTitle}>
+          <h4>CHAPTER 1</h4>
+          <div className={styles.chapter}>
+            <h3>챕터명</h3>
+            <span>
+              <button className={styles.btn}>수정</button>
+              <button className={styles.btn}>삭제</button>
+            </span>
+          </div>
+
+          <div className={styles.class}>
+            <div>
+              <h4>강의명1</h4>
+              <h5>영상길이</h5>
+            </div>
+            <span>
+              <button className={styles.btn}>수정</button>
+              <button className={styles.btn}>삭제</button>
+            </span>
+          </div>
+
+          <div className={styles.addbtnWrapper}>
+            <button className={styles.addbtn}>
+              <FontAwesomeIcon icon={faPencil} className={styles.fontimg} />
+              강의 추가하기
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CurriculumInfo;
