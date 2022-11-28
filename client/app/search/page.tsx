e3bb28e22d3e36334eb854b1e8c47bd177b13ac8
wongee93 <jhwon9304@gmail.com>
2022-11-29 01:21:09 +0900

    separate searchbar from searchpage

--- client/app/search/page.tsx
+++ client/app/search/page.tsx
@@ -1,61 +1,27 @@
-"use client";
-import React, { use, useState } from 'react'
+import React from 'react'
 import styles from './search.module.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import TabNavigator from '../../components/TabNavigator/TabNavigator';
+import SearchBar from '../../components/Search/SearchBar';
 import getSearchData from '../../components/Search/PopularKeyword';
-import { searchApi } from '../../components/Search/searchApi';
 
 // client 페이지
 // input에 value 에 추가
 // params.categoryName으로 받기
 
-const SearchPage = () => {
+const SearchPage = async () => {
 
     // 인기 검색어(무한 fetching 문제 발생...)
-    // const popularKeyword = use(getSearchData());
-
-    // 검색어 
-    const [searchvalue, setSearchvalue] = useState('');
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearchvalue(e.target.value);
-        console.log(searchvalue);
-
-    }
-
-    // 검색 버튼 클릭 시, 내용 담아서 GET 요청 보내기
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        // console.log(searchvalue);
-        // alert(JSON.stringify(searchvalue));
-        searchApi.getSearch(searchvalue);
-    }
+    const popularKeyword = await getSearchData();
 
     return (
         <>
             <div className={styles.searchpageWrapper}>
-                <div className={styles.searchWrapper}>
-                    <div className={styles.searchboxWrapper}>
-                        <form onSubmit={handleSubmit}>
-                            <input
-                                value={searchvalue}
-                                onChange={handleChange}
-                                className={styles.searchbox}
-                                placeholder='관심 클래스 찾기' />
-                            <button className={styles.submitbtn} type='submit'>
-                                <FontAwesomeIcon
-                                    icon={faMagnifyingGlass}
-                                    className={styles.magnifyingglass} />
-                            </button>
-                        </form>
-                    </div>
-                </div>
+
+                <SearchBar />
 
                 <div className={styles.title}>인기순</div>
 
-                {/* {popularKeyword.map((e: any, idx: number) => {
+                {popularKeyword.map((e: any, idx: number) => {
                     return (
                         <ol>
                             <li className={styles.li}>
@@ -64,7 +30,7 @@ const SearchPage = () => {
                             </li>
                         </ol>
                     )
-                })} */}
+                })}
 
             </div>
             <TabNavigator activeLink={'home'} />
--- client/components/Search/PopularKeyword.ts
+++ client/components/Search/PopularKeyword.ts
@@ -1,13 +1,16 @@
 // 인기 검색어 get 함수
 
 const getSearchData = async () => {
-    const res = await fetch('http://localhost:3000/api/popularkeyword');
 
-    if (!res.ok) {
-        throw new Error('failed to data fetch')
+    try {
+        const response = await fetch(`http://localhost:3000/api/popularkeyword`);
+        const { data } = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error(error);
+        return [];
     }
-
-    return await res.json();
 }
 
 export default getSearchData;
