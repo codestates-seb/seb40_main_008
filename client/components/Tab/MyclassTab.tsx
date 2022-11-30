12d6283ec1e0879d08b88306968acb8bf3100b93
wongee93 <jhwon9304@gmail.com>
2022-11-30 17:20:27 +0900

    add searchbar padding

--- client/components/Tab/MyclassTab.tsx
+++ client/components/Tab/MyclassTab.tsx
@@ -7,29 +7,29 @@ import { ICategorySearchResult } from '../../types/category_search/categorySearc
 import HomeClassesSection from '../Card/HomeClassesSection';
 
 interface MyclassTabProps {
-	takingClasses: Array<ICategorySearchResult>;
-	wishClasses: Array<ICategorySearchResult>;
+    takingClasses: Array<ICategorySearchResult>;
+    wishClasses: Array<ICategorySearchResult>;
 }
 
 const MyclassTab = ({ takingClasses, wishClasses }: MyclassTabProps) => {
-	const [tabIndex, setTabIndex] = useState(0);
-	return (
-		<Tabs
-			selectedIndex={tabIndex}
-			onSelect={(tabIndex) => setTabIndex(tabIndex)}
-		>
-			<TabList className={styles.tablist}>
-				<Tab className={styles.tab}>수강중인 클래스</Tab>
-				<Tab className={styles.tab}>찜한 클래스</Tab>
-			</TabList>
-			<TabPanel>
-				<HomeClassesSection contentsList={takingClasses} />
-			</TabPanel>
-			<TabPanel>
-				<HomeClassesSection contentsList={wishClasses} />
-			</TabPanel>
-		</Tabs>
-	);
+    const [tabIndex, setTabIndex] = useState(0);
+    return (
+        <Tabs
+            selectedIndex={tabIndex}
+            onSelect={(tabIndex) => setTabIndex(tabIndex)}
+        >
+            <TabList className={styles.tablist}>
+                <Tab className={styles.tab}>수강중인 클래스</Tab>
+                <Tab className={styles.tab}>찜한 클래스</Tab>
+            </TabList>
+            <TabPanel>
+                <HomeClassesSection contentsList={takingClasses} />
+            </TabPanel>
+            <TabPanel>
+                <HomeClassesSection contentsList={wishClasses} />
+            </TabPanel>
+        </Tabs>
+    );
 };
 
 export default MyclassTab;
