abebc7da7e87844b1c814c2ce8031de81212276f
akdlsz21 <81629070+akdlsz21@users.noreply.github.com>
2022-11-30 14:06:06 +0900

    FE: [Fix] deleted unused files

--- client/utils/helper/extractToken.ts
+++
@@ -1,9 +0,0 @@
-// export function extractToken(cookie: string) {
-	// const indexOfCookieStart =
-	// 	cookie.indexOf('accessToken') + 'accessToken='.length;
-	// const indexOfCookieEnd = cookie.indexOf(';', indexOfCookieStart);
-	// const token = cookie.slice(indexOfCookieStart, indexOfCookieEnd + 1);
-// 	const token = cookie;
-
-// 	return token;
-// }
