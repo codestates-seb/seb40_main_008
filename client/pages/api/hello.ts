2c0a98aa8de7d80a7f5bc6ae0329c31431bfba4f
akdlsz21 <akdlsz21@gmail.com>
2022-11-30 17:19:22 +0900

    deleted debuggin g code

--- client/pages/api/hello.ts
+++ client/pages/api/hello.ts
@@ -5,14 +5,9 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	console.log('req', req.cookies);
 	const token = await getToken({
 		req,
 		secret: process.env.JWT_SECRET as string,
 	});
-
-	// console.log('🚀 ~ file: hello.ts ~ line 6 ~ handler ~ token', token);
-	// console.log('json web token', JSON.stringify(token, null, 2));
-
 	res.status(200).json({ text: 'Hello' });
 }
--- client/utils/helper/backendUserInfo.ts
+++ client/utils/helper/backendUserInfo.ts
@@ -1,36 +1,20 @@
-import IUserInfo from "../../types/user/userinfo";
+import IUserInfo from '../../types/user/userinfo';
 // import { extractToken } from './extractToken';
 
-const getUserInfo = async (cookie: string): Promise<IUserInfo> => {
-  const token = cookie;
-  console.log("token", token);
-  // console.log('cookie', cookie);
-  // console.log(
-  // 	'🚀 ~ file: backendUserInfo.ts ~ line 6 ~ getUserInfo ~ token',
-  // 	token
-  // );
-
-  const res = await fetch("https://pioneroroom.com/auth/userinfo", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
-  const body = await res.json();
-  console.log(
-    "🚀 ~ file: backendUserInfo.ts ~ line 18 ~ getUserInfo ~ body",
-    body
-  );
-  if (!body.data?.userName) {
-    console.log("user info not found");
-    return null;
-  }
-  return body.data;
+const getUserInfo = async (token: string): Promise<IUserInfo> => {
+	const res = await fetch('https://pioneroroom.com/auth/userinfo', {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${token}`,
+		},
+	});
+	const body = await res.json();
+	if (!body.data?.userName) {
+		console.log('user info not found');
+		return null;
+	}
+	return body.data;
 };
 
 export default getUserInfo;
-
-// eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJVU0VSIl0sInVzZXJuYW1lIjoiamh3b245MzA0QGdtYWlsLmNvbSIsInN1YiI6Impod29uOTMwNEBnbWFpbC5jb20iLCJpYXQiOjE2Njk3ODE4NjQsImV4cCI6MTY2OTc4MzY2NH0.g0II9j7VdXIFiHNtQEfCHisCB9v3L69kPueizVcqkR07H9uHhQQpSZ9sV4WJT5uRV5UMSqOsXvk1IRRMACqtaQ
-// eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJVU0VSIl0sInVzZXJuYW1lIjoiamh3b245MzA0QGdtYWlsLmNvbSIsInN1YiI6Impod29uOTMwNEBnbWFpbC5jb20iLCJpYXQiOjE2Njk3ODE4NjQsImV4cCI6MTY2OTc4MzY2NH0.g0II9j7VdXIFiHNtQEfCHisCB9v3L69kPueizVcqkR07H9uHhQQpSZ9sV4WJT5uRV5UMSqOsXvk1IRRMACqtaQ
