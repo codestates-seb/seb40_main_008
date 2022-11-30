44d26d385e5920bf921697dcf42cd73c6baac0da
akdlsz21 <akdlsz21@gmail.com>
2022-11-30 17:03:58 +0900

    myclass tab pages renders classes section grid

--- client/app/myclass/page.tsx
+++ client/app/myclass/page.tsx
@@ -1,49 +1,45 @@
-import React from 'react'
+import React from 'react';
 import styles from './myclass.module.css';
 import TabNavigator from '../../components/TabNavigator/TabNavigator';
 import { ICategorySearchResult } from '../../types/homeScreen/mainVideoContents';
 import HomeClassesSection from '../../components/Card/HomeClassesSection';
 import MyclassTab from '../../components/Tab/MyclassTab';
+const URL = 'https://run.mocky.io/v3/072e5b64-e3fb-4b38-aa50-313b8b680818';
+const getTakingClasses = async (): Promise<Array<ICategorySearchResult>> => {
+	try {
+		// request url : https://pioneroroom.com/auth/takingclass
+		// request url : https://pioneroroom.com/auth/wishclass
+		const response = await fetch(URL);
+		const { contentsList } = await response.json();
+		return contentsList;
+	} catch (error) {
+		console.error(error);
+		return [];
+	}
+};
 
-const getMyclassContents = async (): Promise<Array<ICategorySearchResult>> => {
-
-    try {
-        // request url : https://pioneroroom.com/auth/takingclass
-        // request url : https://pioneroroom.com/auth/wishclass
-        const response = await fetch(
-            `https://run.mocky.io/v3/072e5b64-e3fb-4b38-aa50-313b8b680818`,
-            {
-                next: {
-                    revalidate: 60,
-                },
-            }
-        );
-        const { contentsList } = await response.json();
-        return contentsList;
-
-    } catch (error) {
-        console.error(error);
-        return [];
-    }
+const getWishClasses = async (): Promise<Array<ICategorySearchResult>> => {
+	try {
+		const response = await fetch(URL);
+		const { contentsList } = await response.json();
+		return contentsList.reverse();
+	} catch (error) {
+		console.error(error);
+		return [];
+	}
 };
 
 const MyclassPage = async () => {
+	const takingClasses = await getTakingClasses();
+	const wishClasses = await getWishClasses();
+	return (
+		<>
+			<div className={styles.title}>내 클래스</div>
+			<MyclassTab takingClasses={takingClasses} wishClasses={wishClasses} />
 
-    // const 
-
-    // 탭끼리 눌렀을 때 조건벌로 분기해서 contentsList 받아오기
-    // let contentsList = await getMyclassContents();
-    // console.log(contentsList);
-
-    return (
-        <>
-            <div className={styles.title}>내 클래스</div>
-            <MyclassTab />
-            {/* <HomeClassesSection contentsList={contentsList} /> */}
-
-            <TabNavigator activeLink={'myclass'} />
-        </>
-    )
-}
+			<TabNavigator activeLink={'myclass'} />
+		</>
+	);
+};
 
 export default MyclassPage;
--- client/components/Tab/MyclassTab.tsx
+++ client/components/Tab/MyclassTab.tsx
@@ -1,60 +1,35 @@
-'use client'
-import React, { useState } from 'react'
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+'use client';
+import React, { useState } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import styles from './MyclassTab.module.css';
 import { useRouter } from 'next/navigation';
+import { ICategorySearchResult } from '../../types/category_search/categorySearchType';
+import HomeClassesSection from '../Card/HomeClassesSection';
 
-const MyclassTab = () => {
-
-    const [tabIndex, setTabIndex] = useState(0);
-    // console.log('tabIndex', tabIndex);
-
-    // const router = useRouter();
-    // if (tabIndex === 0) {
-    //     router.push(`/myclass#takingclass`);
-    // } else if (tabIndex === 1) {
-    //     router.push(`/myclass#wishclass`);
-    // }
-
-    return (
-        <Tabs
-            selectedIndex={tabIndex}
-            onSelect={(tabIndex) => setTabIndex(tabIndex)}
-        >
-
-            <TabList className={styles.tablist} >
-                <Tab className={styles.tab} value='takingclass'>수강중인 클래스</Tab>
-                <Tab className={styles.tab} value='wishclass'>찜한 클래스</Tab>
-            </TabList>
-
-            {/* <div className={styles.tabpanelWrapper}> */}
-            <TabPanel>
-                <div
-                    style={{
-                        width: "100%",
-                        padding: "20px",
-                        height: "0px",
-                        border: "1px solid white",
-                        margin: "20px auto",
-                    }}
-                >
-                </div>
-            </TabPanel>
-            <TabPanel>
-                <div
-                    style={{
-                        width: "90%",
-                        padding: "20px",
-                        height: "0px",
-                        border: "1px solid white",
-                        margin: "20px auto",
-                    }}
-                >
-                </div>
-            </TabPanel>
-            {/* </div> */}
-        </Tabs>
-    )
+interface MyclassTabProps {
+	takingClasses: Array<ICategorySearchResult>;
+	wishClasses: Array<ICategorySearchResult>;
 }
+
+const MyclassTab = ({ takingClasses, wishClasses }: MyclassTabProps) => {
+	const [tabIndex, setTabIndex] = useState(0);
+	return (
+		<Tabs
+			selectedIndex={tabIndex}
+			onSelect={(tabIndex) => setTabIndex(tabIndex)}
+		>
+			<TabList className={styles.tablist}>
+				<Tab className={styles.tab}>수강중인 클래스</Tab>
+				<Tab className={styles.tab}>찜한 클래스</Tab>
+			</TabList>
+			<TabPanel>
+				<HomeClassesSection contentsList={takingClasses} />
+			</TabPanel>
+			<TabPanel>
+				<HomeClassesSection contentsList={wishClasses} />
+			</TabPanel>
+		</Tabs>
+	);
+};
 
 export default MyclassTab;
