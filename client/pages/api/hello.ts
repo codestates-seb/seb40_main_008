e9b31a2c923e20d79cbd100ab49f0c6cf6f77f60
akdlsz21 <akdlsz21@gmail.com>
2022-12-02 17:38:42 +0900

    props to image in home screen

--- client/pages/api/hello.ts
+++ client/pages/api/hello.ts
@@ -1,26 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { NextRequest, NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
-import { deleteCookie } from 'cookies-next';
+import { deleteCookie, removeCookies } from 'cookies-next';
 
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	// res.cookies.set('accessToken', '', {
-	// 	httpOnly: false,
-	// 	maxAge: 0, // 0 second hours in seconds
-	// });
-	deleteCookie('accessToken', { res, req });
+	// deleteCookie('accessToken', { res, req });
+	removeCookies('accessToken', { res, req });
 	res.status(200).json({ message: 'success' });
+	console.log('🚀 ~ file: hello.ts:14 ~ res', res);
 	return res;
 }
-// export default async function handler(
-// 	req: NextApiRequest,
-// 	res: NextApiResponse
-// ) {
-// 	console.log('🚀 ~ file: hello.ts:16 ~ req', req);
-// 	res.getHeader
-// 	res.status(200).json({ name: 'John Doe' });
-// 	return res;
-// }
