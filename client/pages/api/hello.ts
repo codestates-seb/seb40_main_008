a401ca06f442dadd64bb1fd1904c58e7a55232a7
akdlsz21 <akdlsz21@gmail.com>
2022-12-02 16:53:06 +0900

    api route for deleting cookie

--- client/pages/api/hello.ts
+++ client/pages/api/hello.ts
@@ -1,19 +1,26 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
+import { deleteCookie } from 'cookies-next';
 
-// export default async function handler(req: NextRequest, res: NextResponse) {
-// 	console.log('🚀 ~ file: hello.ts:6 ~ handler ~ req', req);
-// 	const url = req.nextUrl;
-// 	console.log('🚀 ~ file: hello.ts:8 ~ handler ~ url', url);
-// 	NextResponse.redirect(`${url}`);
-// 	NextResponse.json({ name: 'John Doe' });
-// }
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	console.log('🚀 ~ file: hello.ts:16 ~ req', req);
-	res.status(200).json({ name: 'John Doe' });
+	// res.cookies.set('accessToken', '', {
+	// 	httpOnly: false,
+	// 	maxAge: 0, // 0 second hours in seconds
+	// });
+	deleteCookie('accessToken', { res, req });
+	res.status(200).json({ message: 'success' });
 	return res;
 }
+// export default async function handler(
+// 	req: NextApiRequest,
+// 	res: NextApiResponse
+// ) {
+// 	console.log('🚀 ~ file: hello.ts:16 ~ req', req);
+// 	res.getHeader
+// 	res.status(200).json({ name: 'John Doe' });
+// 	return res;
+// }
