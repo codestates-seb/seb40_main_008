2c492ce4a122a2f4fd4605d4dd1a7670c1a4f8f1
podoco <103816228+podoco@users.noreply.github.com>
2022-12-03 14:33:52 +0900

    global css

--- client/components/HomeNavBar/HomeNavBar.tsx
+++ client/components/HomeNavBar/HomeNavBar.tsx
@@ -1,107 +1,95 @@
-'use client';
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-import { useScrollBar } from '../../hooks/\bScrollBar/UseScrollBar';
-import styles from './HomeNavBar.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { useSession } from 'next-auth/react';
-import IUserInfo from '../../types/user/userinfo';
-import { useHasWindow } from '../../utils/hooks/useHasWindow';
+"use client";
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useScrollBar } from "../../hooks/\bScrollBar/UseScrollBar";
+import styles from "./HomeNavBar.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useSession } from "next-auth/react";
+import IUserInfo from "../../types/user/userinfo";
+import { useHasWindow } from "../../utils/hooks/useHasWindow";
 
 interface HomeNavBarProps {
-	userInfo: IUserInfo | undefined;
+  userInfo: IUserInfo | undefined;
 }
 
 const HomeNavBar = ({ userInfo }: HomeNavBarProps) => {
-	const hasWindow = useHasWindow();
-	const { show } = useScrollBar();
-	if (!hasWindow) {
-		return null;
-	}
+  const { show } = useScrollBar();
 
-	if (typeof window === 'undefined') {
-		return null;
-	}
+  const scrollTopBtn = () => {
+    if (typeof window !== "undefined") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  };
 
-	const scrollTopBtn = () => {
-		if (typeof window !== 'undefined') {
-			window.scrollTo({
-				top: 0,
-				behavior: 'smooth',
-			});
-		}
-	};
+  return (
+    <>
+      {window && window.scrollY < 60 ? (
+        <nav className={styles.firstNav}>
+          <div className={styles.logowrapper}>
+            <Link href={"/categories"}>
+              <FontAwesomeIcon
+                icon={faBars}
+                width={24}
+                className={styles.font}
+              />
+            </Link>
 
-	return (
-		<>
-			{window && window.scrollY < 60 ? (
-				<nav className={styles.firstNav}>
-					<div className={styles.logowrapper}>
-						<Link href={'/categories'}>
-							<FontAwesomeIcon
-								icon={faBars}
-								width={24}
-								className={styles.font}
-							/>
-						</Link>
-
-						<button className={styles.logo} onClick={scrollTopBtn}>
-							CLASS4989
-						</button>
-					</div>
-					<div>
-						{userInfo ? (
-							<Link href={'/mypage'}>
-								<Image
-									className="myimg"
-									alt="myimg"
-									src="/img/myimg.png"
-									width={35}
-									height={35}
-								/>
-							</Link>
-						) : (
-							<Link className={styles.login} href={'/login'}>
-								Login
-							</Link>
-						)}
-					</div>
-				</nav>
-			) : (
-				<nav className={`${show ? styles.nav : styles.change_nav} `}>
-					<div className="logo">
-						<FontAwesomeIcon
-							icon={faBars}
-							width={24}
-							className={styles.font}
-						/>
-						<button className={styles.logo} onClick={scrollTopBtn}>
-							CLASS4989
-						</button>
-					</div>
-					<div>
-						{userInfo ? (
-							<Link href={'/mypage'}>
-								<Image
-									className="myimg"
-									alt="myimg"
-									src="/img/myimg.png"
-									width={35}
-									height={35}
-								/>
-							</Link>
-						) : (
-							<Link className={styles.login} href={'/login'}>
-								Login
-							</Link>
-						)}
-					</div>
-				</nav>
-			)}
-		</>
-	);
+            <button className={styles.logo} onClick={scrollTopBtn}>
+              CLASS4989
+            </button>
+          </div>
+          <div>
+            {userInfo ? (
+              <Link href={"/mypage"}>
+                <Image
+                  className="myimg"
+                  alt="myimg"
+                  src="/img/myimg.png"
+                  width={35}
+                  height={35}
+                />
+              </Link>
+            ) : (
+              <Link className={styles.login} href={"/login"}>
+                Login
+              </Link>
+            )}
+          </div>
+        </nav>
+      ) : (
+        <nav className={`${show ? styles.nav : styles.change_nav} `}>
+          <div className="logo">
+            <FontAwesomeIcon icon={faBars} width={24} className={styles.font} />
+            <button className={styles.logo} onClick={scrollTopBtn}>
+              CLASS4989
+            </button>
+          </div>
+          <div>
+            {userInfo ? (
+              <Link href={"/mypage"}>
+                <Image
+                  className="myimg"
+                  alt="myimg"
+                  src="/img/myimg.png"
+                  width={35}
+                  height={35}
+                />
+              </Link>
+            ) : (
+              <Link className={styles.login} href={"/login"}>
+                Login
+              </Link>
+            )}
+          </div>
+        </nav>
+      )}
+    </>
+  );
 };
 
 export default HomeNavBar;
--- client/hooks/ScrollBar/UseScrollBar.tsx
+++ client/hooks/ScrollBar/UseScrollBar.tsx
@@ -1,35 +1,35 @@
-import { useEffect, useState } from 'react';
-import { useTrottle } from './useTrottle';
+import { useEffect, useState } from "react";
+import { useTrottle } from "./useTrottle";
 
 export const useScrollBar = () => {
-	const [show, setShow] = useState(true);
-	const [lastScrollY, setLastScrollY] = useState(0);
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
 
-	const controlNavbar = () => {
-		if (typeof window !== 'undefined') {
-			if (window.scrollY > lastScrollY) {
-				// if scroll down hide the navbar
-				setShow(false);
-			} else {
-				// if scroll up show the navbar
-				setShow(true);
-			}
-			// remember current page location to use in the next move
-			setLastScrollY(window.scrollY);
-		}
-	};
-	const trottleNavbarControl = useTrottle(controlNavbar, 100);
+  const controlNavbar = () => {
+    if (typeof window !== "undefined") {
+      if (window.scrollY > lastScrollY) {
+        // if scroll down hide the navbar
+        setShow(false);
+      } else {
+        // if scroll up show the navbar
+        setShow(true);
+      }
+      // remember current page location to use in the next move
+      setLastScrollY(window.scrollY);
+    }
+  };
+  const trottleNavbarControl = useTrottle(controlNavbar, 100);
 
-	useEffect(() => {
-		if (typeof window !== 'undefined') {
-			window.addEventListener('scroll', trottleNavbarControl);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", trottleNavbarControl);
 
-			// cleanup function
-			return () => {
-				window.removeEventListener('scroll', trottleNavbarControl);
-			};
-		}
-	}, [lastScrollY, trottleNavbarControl]);
+      // cleanup function
+      return () => {
+        window.removeEventListener("scroll", trottleNavbarControl);
+      };
+    }
+  }, []);
 
-	return { show };
+  return { show };
 };
