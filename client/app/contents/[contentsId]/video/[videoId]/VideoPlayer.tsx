e0565daab72621cf49ea9867050a02d79dd36048
akdlsz21 <akdlsz21@gmail.com>
2022-12-03 21:05:40 +0900

    video player fits parent layout

--- client/app/contents/[contentsId]/video/[videoId]/VideoPlayer.tsx
+++ client/app/contents/[contentsId]/video/[videoId]/VideoPlayer.tsx
@@ -4,7 +4,8 @@ import ReactPlayer from 'react-player';
 import { OnProgressProps } from 'react-player/base';
 import { useHasWindow } from '../../../../../utils/hooks/useHasWindow';
 import styles from './VideoPage.module.css';
-import { Player } from 'video-react';
+import { Player, ControlBar } from 'video-react';
+import './video.css';
 interface VideoPlayerProps {
 	url: string;
 	videoRef: any;
@@ -35,22 +36,44 @@ const VideoPlayer = ({ url, videoRef, setTime }: VideoPlayerProps) => {
 		setTime(`${minutesString}:${secondsString}`);
 	};
 
-	return <Player src={url} ref={videoRef} />;
-
 	return (
-		<ReactPlayer
-			ref={videoRef}
-			url={url}
-			muted={true}
-			controls={true}
-			playing={true}
-			className={styles.videoPlayer}
-			width="100%"
-			height="360px"
-			progressInterval={1000}
-			onProgress={handleProgress}
-		/>
+		<div
+			style={{
+				border: '1px solid white',
+				width: '100%',
+				height: '360px',
+			}}
+		>
+			<Player
+				playsInline={true}
+				ref={videoRef}
+				fluid={false}
+				height={'100%'}
+				autoPlay={true}
+				muted={false}
+				preload="auto"
+				src={url}
+				width={'100%'}
+			>
+				<ControlBar autoHide={false} />
+			</Player>
+		</div>
 	);
+
+	// return (
+	// 	<ReactPlayer
+	// 		ref={videoRef}
+	// 		url={url}
+	// 		muted={true}
+	// 		controls={true}
+	// 		playing={true}
+	// 		className={styles.videoPlayer}
+	// 		width="100%"
+	// 		height="360px"
+	// 		progressInterval={1000}
+	// 		onProgress={handleProgress}
+	// 	/>
+	// );
 };
 
 export default VideoPlayer;
