5c02ea329a56fe8eb692756833ed484f7b8cda78
akdlsz21 <81629070+akdlsz21@users.noreply.github.com>
2022-12-03 19:51:41 +0900

    FE: [Fix] content page with uploadclassId typecheck fixed

--- client/app/charge/page.tsx
+++ client/app/charge/page.tsx
@@ -1,56 +1,72 @@
-import React from "react";
-import BaseNavbar from "../../components/BaseNavBar/BaseNavbar";
-import styles from "./ChargeCoin.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoins } from "@fortawesome/free-solid-svg-icons";
-import OrangeButton from "../../components/Buttons/orangeButton";
-import verifyLogin from "../../utils/VerifyLogin";
+import React from 'react';
+import BaseNavbar from '../../components/BaseNavBar/BaseNavbar';
+import styles from './ChargeCoin.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoins } from '@fortawesome/free-solid-svg-icons';
+import OrangeButton from '../../components/Buttons/orangeButton';
+import ChargeButton from './ChargeButton';
 
-const ChargeCoin = async () => {
-  const userInfo = await verifyLogin();
-  return (
-    <>
-      <BaseNavbar />
-      <div className={styles.TitleWrapper}>
-        <div style={{ marginBottom: "20px", fontSize: "26px", fontWeight: "bold" }}>코인 충전</div>
-        <div style={{ fontSize: "18px" }}>보유 코인 <span style={{ fontSize: "18px", marginLeft: "20px" }}>{userInfo?.totalCoin}원</span></div>
-      </div>
+const ChargeCoin = () => {
+	return (
+		<>
+			<BaseNavbar />
+			<div className={styles.TitleWrapper}>
+				<h1 style={{ padding: '10px' }}>코인 충전</h1>
+				<h2>보유 코인 원</h2>
+			</div>
+			<div className={styles.line}></div>
 
-      <div className={styles.ChargeWrapper}>
-        <div className={styles.charge}>
-          <div className={styles.coinwrap}>
-            <FontAwesomeIcon icon={faCoins} width={24} className={styles.coinfont} />
-            <div className={styles.price}>5,000원</div>
-          </div>
-          <button className={styles.chargeBtn}> 충전하기 </button>
-        </div>
+			<div className={styles.ChargeWrapper}>
+				<div className={styles.charge}>
+					<div className={styles.coinwrap}>
+						<FontAwesomeIcon
+							icon={faCoins}
+							width={24}
+							className={styles.coinfont}
+						/>
+						<h2>5000원</h2>
+					</div>
+					<ChargeButton amount={'FIVE'} />
+				</div>
 
-        <div className={styles.charge}>
-          <div className={styles.coinwrap}>
-            <FontAwesomeIcon icon={faCoins} width={24} className={styles.coinfont} />
-            <div className={styles.price}>10,000원</div>
-          </div>
-          <button className={styles.chargeBtn}> 충전하기 </button>
-        </div>
+				<div className={styles.charge}>
+					<div className={styles.coinwrap}>
+						<FontAwesomeIcon
+							icon={faCoins}
+							width={24}
+							className={styles.coinfont}
+						/>
+						<h2>10000원</h2>
+					</div>
+					<ChargeButton amount={'TEN'} />
+				</div>
 
-        <div className={styles.charge}>
-          <div className={styles.coinwrap}>
-            <FontAwesomeIcon icon={faCoins} width={24} className={styles.coinfont} />
-            <div className={styles.price}>20,000원</div>
-          </div>
-          <button className={styles.chargeBtn}> 충전하기 </button>
-        </div>
-        <div className={styles.charge}>
-          <div className={styles.coinwrap}>
-            <FontAwesomeIcon icon={faCoins} width={24} className={styles.coinfont} />
-            <div className={styles.price}>50,000원</div>
-          </div>
-          <button className={styles.chargeBtn}> 충전하기 </button>
-        </div>
-      </div>
+				<div className={styles.charge}>
+					<div className={styles.coinwrap}>
+						<FontAwesomeIcon
+							icon={faCoins}
+							width={24}
+							className={styles.coinfont}
+						/>
+						<h2>20000원</h2>
+					</div>
+					<ChargeButton amount={'TWENTY'} />
+				</div>
+				<div className={styles.charge}>
+					<div className={styles.coinwrap}>
+						<FontAwesomeIcon
+							icon={faCoins}
+							width={24}
+							className={styles.coinfont}
+						/>
+						<h2>50000원</h2>
+					</div>
+					<ChargeButton amount={'FIFTY'} />
+				</div>
+			</div>
 
-      <OrangeButton name={"충전 내역보기"}></OrangeButton>
-    </>
-  );
+			<OrangeButton name={'충전 내역보기'}></OrangeButton>
+		</>
+	);
 };
 export default ChargeCoin;
--- client/types/contents.ts
+++ client/types/contents.ts
@@ -1,30 +1,30 @@
 export interface IContent {
-  contentsId: number;
-  title: string;
-  thumbnail: string;
-  likesCount: number;
-  categories: string;
-  details: string;
-  grade: number;
-  price: number;
-  role: "creator" | "Unpaid_customer" | "Paid_customer";
-  wished: boolean;
-  tutorName: string;
-  tutorDetail: string;
+	contentsId: number;
+	title: string;
+	thumbnail: string;
+	likesCount: number;
+	categories: string;
+	details: string;
+	grade: number;
+	price: number;
+	role: 'creator' | 'Unpaid_customer' | 'Paid_customer';
+	wished: boolean;
+	tutorName: string;
+	tutorDetail: string;
 }
 
 export interface Curriculum {
-  curriculumInfo: ICurriculumContent[];
+	curriculumInfo?: ICurriculumContent[];
 }
 export interface ICurriculumContent {
-  chapterId: number;
-  chapterOrder: string;
-  title: string;
-  thumbnail: string;
-  uploadClassList: UploadClassList[];
+	chapterId: number;
+	chapterOrder: string;
+	title: string;
+	thumbnail: string;
+	uploadClassList: UploadClassList[];
 }
 
 export interface UploadClassList {
-  uploadClassId: number;
-  title: string;
+	uploadClassId: number;
+	title: string;
 }
