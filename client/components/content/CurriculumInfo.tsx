78dd3c20b989fcec273adc49d790c128cc6c4970
wongee93 <jhwon9304@gmail.com>
2022-12-03 16:29:48 +0900

    add no curriculum screen

--- client/components/content/CurriculumInfo.tsx
+++ client/components/content/CurriculumInfo.tsx
@@ -22,106 +22,109 @@ const CurriculumInfo = ({
   const router = useRouter();
   return (
     <>
-      {curriculumInfo &&
+      {curriculumInfo.length === 0 ?
+        <div className={styles.nocurriculumWrapper}>업로드된 커리큘럼이 없습니다.</div>
+        :
         curriculumInfo.map((e, index) => (
           <div key={index} className={styles.Wrapper}>
-            <div className={styles.thumbnail}>
-              <Image src={e.thumbnail} alt={e.title} fill={true} style={{ objectFit: 'cover' }} />
-            </div>
-            <div className={styles.chapterWrapper}>
-              <div className={styles.chapterTitle}>
-                <p style={{ fontSize: '15px', fontWeight: 'bold' }}>{e.chapterOrder}</p>
-                <div className={styles.chapter}>
-                  <p style={{ fontSize: '22px', fontWeight: 'bold' }}>{e.title}</p>
-                  <span>
-                    {role == "creator" ? (
-                      <>
-                        <Link
-                          href={{
-                            pathname: "/upload/chapter",
-                            query: {
-                              slug: "edit",
-                              chapterId: e.chapterId,
-                              thumbnail: e.thumbnail,
-                              title: e.title,
-                              chapterOrder: e.chapterOrder,
-                            },
-                          }}
-                        >
-                          <button className={styles.btn}>수정</button>
-                        </Link>
+            <div>
+              <div className={styles.thumbnail}>
+                <Image src={e.thumbnail} alt={e.title} fill={true} style={{ objectFit: 'cover' }} />
+              </div>
+              <div className={styles.chapterWrapper}>
+                <div className={styles.chapterTitle}>
+                  <p style={{ fontSize: '15px', fontWeight: 'bold' }}>{e.chapterOrder}</p>
+                  <div className={styles.chapter}>
+                    <p style={{ fontSize: '22px', fontWeight: 'bold' }}>{e.title}</p>
+                    <div>
+                      {role == "creator" ? (
+                        <>
+                          <Link
+                            href={{
+                              pathname: "/upload/chapter",
+                              query: {
+                                slug: "edit",
+                                chapterId: e.chapterId,
+                                thumbnail: e.thumbnail,
+                                title: e.title,
+                                chapterOrder: e.chapterOrder,
+                              },
+                            }}
+                          >
+                            <button className={styles.btn}>수정</button>
+                          </Link>
 
-                        <Curriculumdelete
-                          url={`https://pioneroroom.com/auth/contents/chapter/`}
-                          Id={e.chapterId}
-                        />
-                      </>
+                          <Curriculumdelete
+                            url={`https://pioneroroom.com/auth/contents/chapter/`}
+                            Id={e.chapterId}
+                          />
+                        </>
+                      ) : (
+                        ""
+                      )}
+                    </div>
+                  </div>
+
+                  {e.uploadClassList.map((e, index) => (
+                    <>
+                      <div key={index} className={styles.class}>
+                        <div>
+                          <Link
+                            href={{
+                              pathname: `/contents/${contentsId}/video/${e.uploadClassId}`,
+                            }}
+                          >
+                            <h4>{e.title}</h4>
+                          </Link>
+                        </div>
+                        <div>
+                          {role == "creator" ? (
+                            <>
+                              <Link
+                                href={{
+                                  pathname: "/upload/class",
+                                  query: { slug: "edit" },
+                                }}
+                              >
+                                <button className={styles.btn}>수정</button>
+                              </Link>
+
+                              <Curriculumdelete
+                                url={`https://pioneroroom.com/auth/chapter/lecture/`}
+                                Id={e.uploadClassId}
+                              />
+                            </>
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                      </div>
+                    </>
+                  ))}
+
+                  <div className={styles.addbtnWrapper}>
+                    {role == "creator" ? (
+                      <Link
+                        href={{
+                          pathname: "/upload/class",
+                          query: {
+                            chapterId: e.chapterId,
+                            contentsId: contentsId,
+                          },
+                        }}
+                      >
+                        <button className={styles.addbtn}>
+                          <FontAwesomeIcon
+                            icon={faPencil}
+                            className={styles.fontimg}
+                          />
+                          강의 추가하기
+                        </button>
+                      </Link>
                     ) : (
                       ""
                     )}
-                  </span>
-                </div>
-
-                {e.uploadClassList.map((e, index) => (
-                  <>
-                    <div key={index} className={styles.class}>
-                      <div>
-                        <Link
-                          href={{
-                            pathname: "/video",
-                            query: { classID: e.uploadClassId },
-                          }}
-                        >
-                          <h4>{e.title}</h4>
-                        </Link>
-                      </div>
-                      <div>
-                        {role == "creator" ? (
-                          <>
-                            <Link
-                              href={{
-                                pathname: "/upload/class",
-                                query: { slug: "edit" },
-                              }}
-                            >
-                              <button className={styles.btn}>수정</button>
-                            </Link>
-
-                            <Curriculumdelete
-                              url={`https://pioneroroom.com/auth/chapter/lecture/`}
-                              Id={e.uploadClassId}
-                            />
-                          </>
-                        ) : (
-                          ""
-                        )}
-                      </div>
-                    </div>
-                  </>
-                ))}
-
-                <div className={styles.addbtnWrapper}>
-                  {role == "creator" ? (
-                    <Link
-                      href={{
-                        pathname: "/upload/class",
-                        query: {
-                          chapterId: e.chapterId,
-                          contentsId: contentsId,
-                        },
-                      }}
-                    >
-                      <button className={styles.addbtn}>
-                        <FontAwesomeIcon
-                          icon={faPencil}
-                          className={styles.fontimg}
-                        />
-                        강의 추가하기
-                      </button>
-                    </Link>
-                  ) : (
-                    ""
-                  )}
+                  </div>
                 </div>
               </div>
             </div>
