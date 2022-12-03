27e3c63c30a037ef36ed347902b0b39e671ab30b
akdlsz21 <akdlsz21@gmail.com>
2022-12-03 19:48:17 +0900

    content page uploadclassId type checking added

--- client/app/charge/ChargeButton.tsx
+++ client/app/charge/ChargeButton.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { getCookie } from 'cookies-next';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 import styles from './ChargeCoin.module.css';
 interface Props {
@@ -7,21 +8,30 @@ interface Props {
 }
 
 const ChargeButton = ({ amount }: Props) => {
+	const router = useRouter();
 	const handleCharge = async () => {
-		const token = getCookie('accessToken');
+		let formData = new FormData();
+		formData.append('chargeAmount', amount);
 
+		const token = getCookie('accessToken');
 		const res = await fetch('https://pioneroroom.com/auth/coincharge/ready', {
 			method: 'POST',
 			headers: {
 				Authorization: `Bearer ${token}`,
 			},
-			body: JSON.stringify({
-				chargeAmount: amount,
-			}),
+			body: formData,
 		});
-		console.log('🚀 ~ file: ChargeButton.tsx:24 ~ handleCharge ~ res', res);
+		const { data } = await res.json();
+		console.log(
+			'🚀 ~ file: ChargeButton.tsx:24 ~ handleCharge ~ data ',
+			data
+		);
+		if (window.innerWidth > 768) {
+			window.location = data.next_redirect_pc_url;
+		} else {
+			window.location = data.next_redirect_mobile_url;
+		}
 	};
-
 	return (
 		<button className={styles.chargeBtn} onClick={handleCharge}>
 			충전하기
