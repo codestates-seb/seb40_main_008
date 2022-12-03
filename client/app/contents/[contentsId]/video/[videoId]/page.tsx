b342ffcbf697c86a1102ec1ebd5879144b432113
akdlsz21 <akdlsz21@gmail.com>
2022-12-03 15:24:35 +0900

    video page not fetching dummy data anymore

--- client/app/contents/[contentsId]/video/[videoId]/page.tsx
+++ client/app/contents/[contentsId]/video/[videoId]/page.tsx
@@ -13,18 +13,16 @@ interface VideoIdPageProps {
 const getVideoPageContent = async (contentsId: string, videoId: string) => {
 	const token = cookies().get('accessToken')?.value;
 	try {
-		// const res = await fetch(
-		// 	`https://pioneroroom.com/auth/contents/${contentsId}/video/${videoId}`,
-		// 	{
-		// 		method: 'GET',
-		// 		headers: {
-		// 			Authorization: `Bearer ${token}`,
-		// 		},
-		// 	}
-		// );
 		const res = await fetch(
-			`https://run.mocky.io/v3/22e05b93-2b82-42ba-86d6-7deeb33755c7`
+			`https://pioneroroom.com/auth/contents/${contentsId}/video/${videoId}`,
+			{
+				method: 'GET',
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			}
 		);
+
 		const data = await res.json();
 		return data;
 	} catch (error) {
