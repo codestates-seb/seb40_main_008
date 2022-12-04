87c182747d956ae8b87dce41041bf71a1d3995a5
Mingi Jeon <103816228+podoco@users.noreply.github.com>
2022-12-04 15:52:56 +0900

    FE:[Fix]contents/css (#116)
    * 쓰이지 않는 카테고리name회색처리
    
    * 카테고리 이름 수정
    
    * 카테고리 네브바
    
    * 인풋창&navbar css
    
    * 인풋 validate
    
    * css warning 수정

--- client/app/contents/[contentsId]/page.tsx
+++ client/app/contents/[contentsId]/page.tsx
@@ -1,55 +1,55 @@
-import React from 'react';
-import BaseNavbar from '../../../components/BaseNavBar/BaseNavbar';
-import ContentInfo from '../../../components/content/ContentInfo';
-import ContentTabs from '../../../components/content/ContentTabs';
-import { IContent, ICurriculumContent } from '../../../types/contents';
-import { cookies } from 'next/headers';
-import { ICategorySearchResult } from '../../../types/category_search/categorySearchType';
-import { ILoopIDList } from '../../../types/detailedContentIdListType';
-import verifyLogin from '../../../utils/VerifyLogin';
+import React from "react";
+import BaseNavbar from "../../../components/BaseNavBar/BaseNavbar";
+import ContentInfo from "../../../components/content/ContentInfo";
+import ContentTabs from "../../../components/content/ContentTabs";
+import { IContent, ICurriculumContent } from "../../../types/contents";
+import { cookies } from "next/headers";
+import { ICategorySearchResult } from "../../../types/category_search/categorySearchType";
+import { ILoopIDList } from "../../../types/detailedContentIdListType";
+import verifyLogin from "../../../utils/VerifyLogin";
 
 const getContentInfo = async (contentsId: string): Promise<IContent> => {
-	const token = cookies().get('accessToken')?.value;
-	const res = await fetch(`https://pioneroroom.com/contents/${contentsId}`, {
-		method: 'GET',
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	});
-	const { contentInfo } = await res.json();
-	return contentInfo;
+  const token = cookies().get("accessToken")?.value;
+  const res = await fetch(`https://pioneroroom.com/contents/${contentsId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const { contentInfo } = await res.json();
+  return contentInfo;
 };
 
 const getCurriculum = async (
-	contentsId: string
+  contentsId: string
 ): Promise<Array<ICurriculumContent>> => {
-	const token = cookies().get('accessToken')?.value;
-	const res = await fetch(`https://pioneroroom.com/contents/${contentsId}`, {
-		method: 'GET',
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	});
-	const { curriculumInfo } = await res.json();
-	return curriculumInfo;
+  const token = cookies().get("accessToken")?.value;
+  const res = await fetch(`https://pioneroroom.com/contents/${contentsId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const { curriculumInfo } = await res.json();
+  return curriculumInfo;
 };
 
 const ContentsIdPage = async ({ params }: any) => {
-	const contentInfo = await getContentInfo(params.contentsId);
-	const curriculumInfo = await getCurriculum(params.contentsId);
-	const userInfo = await verifyLogin();
-	const uploadclassId = getUploadClassId(curriculumInfo);
-	return (
-		<>
-			<BaseNavbar />
-			<ContentInfo uploadclassId={uploadclassId} contentInfo={contentInfo} />
-			<ContentTabs
-			userInfo={userInfo}
-				contentInfo={contentInfo}
-				curriculumInfo={curriculumInfo}
-			/>
-		</>
-	);
+  const contentInfo = await getContentInfo(params.contentsId);
+  const curriculumInfo = await getCurriculum(params.contentsId);
+  const userInfo = await verifyLogin();
+  const uploadclassId = getUploadClassId(curriculumInfo);
+  return (
+    <>
+      <BaseNavbar />
+      <ContentInfo uploadclassId={uploadclassId} contentInfo={contentInfo} />
+      <ContentTabs
+        userInfo={userInfo}
+        contentInfo={contentInfo}
+        curriculumInfo={curriculumInfo}
+      />
+    </>
+  );
 };
 
 // export async function generateStaticParams() {
@@ -66,7 +66,7 @@ const ContentsIdPage = async ({ params }: any) => {
 export default ContentsIdPage;
 
 function getUploadClassId(curriculumInfo: ICurriculumContent[]) {
-	return curriculumInfo[0]?.uploadClassList[0]
-		? curriculumInfo[0].uploadClassList[0].uploadClassId
-		: null;
+  return curriculumInfo[0]?.uploadClassList[0]
+    ? curriculumInfo[0].uploadClassList[0].uploadClassId
+    : null;
 }
--- client/app/upload/class/page.tsx
+++ client/app/upload/class/page.tsx
@@ -1,166 +1,170 @@
-'use client';
-import styles from './uploadClass.module.css';
-import { useState } from 'react';
-import OrangeButton from '../../../components/Buttons/orangeButton';
-import BaseNavbar from '../../../components/BaseNavBar/BaseNavbar';
-import { initialLecture, UploadLectureType } from '../../../types/uploadclass';
-import { useSearchParams } from 'next/navigation';
-import { getCookie } from 'cookies-next';
-import { useRouter } from 'next/navigation';
+"use client";
+import styles from "./uploadClass.module.css";
+import { useState } from "react";
+import OrangeButton from "../../../components/Buttons/orangeButton";
+import BaseNavbar from "../../../components/BaseNavBar/BaseNavbar";
+import { initialLecture, UploadLectureType } from "../../../types/uploadclass";
+import { useSearchParams } from "next/navigation";
+import { getCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
 
 const formData = new FormData();
 
 const UploadClassPage = () => {
-	const token = getCookie('accessToken');
-	const searchParams = useSearchParams();
-	const query = searchParams.get('slug');
-	const chapterId = searchParams.get('chapterId');
-	const contentsId = searchParams.get('contentsId');
-	const router = useRouter();
-	console.log(query);
+  const token = getCookie("accessToken");
+  const searchParams = useSearchParams();
+  const query = searchParams.get("slug");
+  const chapterId = searchParams.get("chapterId");
+  const contentsId = searchParams.get("contentsId");
+  const router = useRouter();
+  console.log(query);
 
-	const [file, setFile] = useState<any>();
-	const [docfile, setDocFile] = useState<any>();
+  const [file, setFile] = useState<any>();
+  const [docfile, setDocFile] = useState<any>();
 
-	const [values, setValues] = useState<UploadLectureType>(initialLecture);
+  const [values, setValues] = useState<UploadLectureType>(initialLecture);
 
-	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setValues({
-			...values,
-			[e.target.name]: e.target.value,
-		});
-	};
-	const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-		setValues({
-			...values,
-			[e.target.name]: e.target.value,
-		});
-	};
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		alert(JSON.stringify(values, null, 2));
-		formData.append('title', values.title);
-		formData.append('details', values.details);
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert(JSON.stringify(values, null, 2));
+    formData.append("title", values.title);
+    formData.append("details", values.details);
 
-		console.log('formData:video:: ', formData.getAll('videoFile'));
-		console.log('formData:docs:: ', formData.getAll('docsFile'));
-		console.log('formData:title:: ', formData.getAll('title'));
+    console.log("formData:video:: ", formData.getAll("videoFile"));
+    console.log("formData:docs:: ", formData.getAll("docsFile"));
+    console.log("formData:title:: ", formData.getAll("title"));
 
-		fetch(`https://pioneroroom.com/auth/chapter/lecture/${chapterId}`, {
-			method: 'POST',
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-			body: formData,
-		}).then((res) => {
-			if (res.ok) {
-				formData.delete('title');
-				formData.delete('details');
-				formData.delete('videoFile');
-				formData.delete('docsFile');
-				router.push(`/contents/${contentsId}`);
-			}
-		});
-	};
+    fetch(`https://pioneroroom.com/auth/chapter/lecture/${chapterId}`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    }).then((res) => {
+      if (res.ok) {
+        formData.delete("title");
+        formData.delete("details");
+        formData.delete("videoFile");
+        formData.delete("docsFile");
+        router.push(`/contents/${contentsId}`);
+      }
+    });
+  };
 
-	router.prefetch(`/contents/${contentsId}`);
+  const uploadVideofile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const fileList = e.target.files;
 
-	const uploadVideofile = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const fileList = e.target.files;
+    if (fileList && fileList[0]) {
+      setFile(fileList[0].name);
+      formData.append("videoFile", fileList[0]);
+      setValues({
+        ...values,
+        videoFile: fileList[0],
+      });
 
-		if (fileList && fileList[0]) {
-			setFile(fileList[0].name);
-			formData.append('videoFile', fileList[0]);
-			setValues({
-				...values,
-				videoFile: fileList[0],
-			});
+      console.log("fileList[0]1", fileList[0].name);
+    }
+  };
 
-			console.log('fileList[0]1', fileList[0].name);
-		}
-	};
+  const uploadDocsfile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const fileList = e.target.files;
 
-	const uploadDocsfile = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const fileList = e.target.files;
+    if (fileList && fileList[0]) {
+      setDocFile(fileList[0].name);
+      formData.append("docsFile", fileList[0]);
+      setValues({
+        ...values,
+        docsFile: fileList[0],
+      });
 
-		if (fileList && fileList[0]) {
-			setDocFile(fileList[0].name);
-			formData.append('docsFile', fileList[0]);
-			setValues({
-				...values,
-				docsFile: fileList[0],
-			});
+      console.log("fileList[0]2", fileList[0].name);
+    }
+  };
 
-			console.log('fileList[0]2', fileList[0].name);
-		}
-	};
+  return (
+    <>
+      <BaseNavbar name={"강의자료 올리기"} page={"back"} />
+      <section className={styles.uploadpage}>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <h1 className={styles.title}>강의</h1>
 
-	return (
-		<>
-			<BaseNavbar />
-			<section className={styles.uploadpage}>
-				<form onSubmit={handleSubmit} className={styles.form}>
-					<h1 className={styles.title}>강의</h1>
+          <p className={styles.classtitle}>강의명</p>
+          <input
+            required
+            type="text"
+            name="title"
+            onChange={handleChange}
+            className={styles.chapternameinput}
+          ></input>
 
-					<p className={styles.classtitle}>강의명</p>
-					<input
-						type="text"
-						name="title"
-						onChange={handleChange}
-						className={styles.chapternameinput}
-					></input>
+          <h3 className={styles.title}>강의 업로드</h3>
+          <div className={styles.filebox}>
+            <input
+              required
+              className={styles.uploadname}
+              value={file}
+              placeholder="첨부파일"
+            />
+            <label htmlFor="file">파일찾기</label>
+            <input
+              required
+              accept="video/*"
+              name="videoFile"
+              onChange={uploadVideofile}
+              type="file"
+              id="file"
+            ></input>
+          </div>
 
-					<h3 className={styles.title}>강의 업로드</h3>
-					<div className={styles.filebox}>
-						<input
-							className={styles.uploadname}
-							value={file}
-							placeholder="첨부파일"
-						/>
-						<label htmlFor="file">파일찾기</label>
-						<input
-							accept="video/*"
-							name="videoFile"
-							onChange={uploadVideofile}
-							type="file"
-							id="file"
-						></input>
-					</div>
+          <h1 className={styles.lecturetitle}>수업자료</h1>
 
-					<h1 className={styles.lecturetitle}>수업자료</h1>
+          <p className={styles.classtitle}>내용</p>
+          <textarea
+            required
+            name="details"
+            onChange={handleTextChange}
+            className={styles.lectureinput}
+          ></textarea>
 
-					<p className={styles.classtitle}>내용</p>
-					<textarea
-						name="details"
-						onChange={handleTextChange}
-						className={styles.lectureinput}
-					></textarea>
-
-					<div className={styles.filebox}>
-						<input
-							className={styles.uploadname}
-							value={docfile}
-							placeholder="첨부파일"
-						/>
-						<label htmlFor="file2">파일찾기</label>
-						<input
-							accept=".pdf, .text/plain"
-							name="docsFile"
-							onChange={uploadDocsfile}
-							type="file"
-							id="file2"
-						></input>
-					</div>
-					{query == 'edit' ? (
-						<OrangeButton type={'submit'} name={'수정하기'} />
-					) : (
-						<OrangeButton type={'submit'} name={'올리기'} />
-					)}
-				</form>
-			</section>
-		</>
-	);
+          <div className={styles.filebox}>
+            <input
+              required
+              className={styles.uploadname}
+              value={docfile}
+              placeholder="첨부파일"
+            />
+            <label htmlFor="file2">파일찾기</label>
+            <input
+              required
+              accept=".pdf, .text/plain"
+              name="docsFile"
+              onChange={uploadDocsfile}
+              type="file"
+              id="file2"
+            ></input>
+          </div>
+          {query == "edit" ? (
+            <OrangeButton type={"submit"} name={"수정하기"} />
+          ) : (
+            <OrangeButton type={"submit"} name={"올리기"} />
+          )}
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default UploadClassPage;
