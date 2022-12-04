6b051d06ede1cec227cbf1bbbab191863ee00bb3
akdlsz21 <akdlsz21@gmail.com>
2022-12-04 14:59:28 +0900

    json body validity check before returning

--- client/components/Buttons/ContentCardWishBtn.tsx
+++ client/components/Buttons/ContentCardWishBtn.tsx
@@ -1,48 +1,41 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import styles from "../content/ContentInfo.module.css";
-import { getCookie } from "cookies-next";
+'use client';
+import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import styles from '../content/ContentInfo.module.css';
+import { getCookie } from 'cookies-next';
 interface ContentCardWishProps {
-  contentId: number;
+	contentId: number;
 }
 
 export const ContentCardWishBtn = (props: ContentCardWishProps) => {
-  const { contentId } = props;
-  const token = getCookie("accessToken");
-  const [wish, setWish] = useState(false);
+	const { contentId } = props;
+	const token = getCookie('accessToken');
+	const [wish, setWish] = useState(false);
 
-  const handleWishCheck = () => {
-    setWish(!wish);
-  };
+	const handleWishCheck = () => {
+		setWish(!wish);
+	};
 
-  useEffect(() => {
-    fetch(`https://pioneroroom.com/auth/${contentId}/wish`, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((res) => {
-        res.json();
-      })
-      .then((data) => {
-        console.log("성공", data);
-      })
-      .catch((error) => {});
-  }, [wish]);
+	useEffect(() => {
+		fetch(`https://pioneroroom.com/auth/${contentId}/wish`, {
+			method: 'POST',
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		}).catch((error) => {});
+	}, [wish]);
 
-  return (
-    <>
-      <button onClick={handleWishCheck} className={styles.zzimbtn}>
-        <FontAwesomeIcon
-          width={24}
-          icon={faCartShopping}
-          className={`${wish ? styles.icon : styles.clickicon}`}
-        />
-        찜하기
-      </button>
-    </>
-  );
+	return (
+		<>
+			<button onClick={handleWishCheck} className={styles.zzimbtn}>
+				<FontAwesomeIcon
+					width={24}
+					icon={faCartShopping}
+					className={`${wish ? styles.icon : styles.clickicon}`}
+				/>
+				찜하기
+			</button>
+		</>
+	);
 };
