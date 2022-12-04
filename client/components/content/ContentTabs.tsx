339e07cf3aa6e7787719d0647fa261f4f1cd7f97
akdlsz21 <81629070+akdlsz21@users.noreply.github.com>
2022-12-04 15:13:24 +0900

    FE: [Refactor] User blocked to access curriculum when not logged in  (#115)
    * userinfo passed through contents page for accessibility
    
    * json body validity check before returning

--- client/components/content/ContentTabs.tsx
+++ client/components/content/ContentTabs.tsx
@@ -1,69 +1,82 @@
-"use client";
-import React, { useState } from "react";
-import styles from "./ContentTabs.module.css";
-import CurriculumInfo from "./CurriculumInfo";
-import { IContent, ICurriculumContent } from "../../types/contents";
-import { CustomTab } from "../Tab/CustomTab";
+'use client';
+import React, { useState } from 'react';
+import styles from './ContentTabs.module.css';
+import CurriculumInfo from './CurriculumInfo';
+import { IContent, ICurriculumContent } from '../../types/contents';
+import { CustomTab } from '../Tab/CustomTab';
+import IUserInfo from '../../types/user/userinfo';
 
 interface ContentTabsProps {
-  contentInfo: IContent;
-  curriculumInfo: ICurriculumContent[];
+	contentInfo: IContent;
+	curriculumInfo: ICurriculumContent[];
+	userInfo: IUserInfo;
 }
 
-const ContentTabs = ({ contentInfo, curriculumInfo }: ContentTabsProps) => {
-  return (
-    <>
-      <CustomTab
-        tabs={["강의소개", "강사소개", "커리큘럼"]}
-        contents={[
-          <>
-            {contentInfo?.details.length === 0 ? (
-              <div className={styles.tabpannel}>강의 소개가 없습니다.</div>
-            ) : (
-              <div
-                style={{
-                  width: "90%",
-                  padding: "20px",
-                  height: "100%",
-                  border: "1px solid white",
-                  margin: "60px auto 20px auto",
-                }}
-              >
-                {contentInfo?.details}
-              </div>
-            )}
-          </>,
-          <>
-            {contentInfo?.details.length === 0 ? (
-              <div className={styles.tabpannel}>강사 소개가 없습니다.</div>
-            ) : (
-              <div
-                style={{
-                  width: "90%",
-                  padding: "20px",
-                  height: "100%",
-                  border: "1px solid white",
-                  margin: "60px auto 20px auto",
-                }}
-              >
-                {contentInfo?.tutorDetail}
-              </div>
-            )}
-          </>,
-          <>
-            {contentInfo?.details.length === 0 ? (
-              <div className={styles.tabpannel}>커리큘럼이 없습니다.</div>
-            ) : (
-              <CurriculumInfo
-                role={contentInfo?.role}
-                contentsId={contentInfo?.contentsId}
-                curriculumInfo={curriculumInfo}
-              />
-            )}
-          </>,
-        ]}
-      />
-    </>
-  );
+const ContentTabs = ({
+	contentInfo,
+	curriculumInfo,
+	userInfo,
+}: ContentTabsProps) => {
+	return (
+		<>
+			<CustomTab
+				tabs={['강의소개', '강사소개', '커리큘럼']}
+				contents={[
+					<>
+						{contentInfo?.details.length === 0 ? (
+							<div className={styles.tabpannel}>
+								강의 소개가 없습니다.
+							</div>
+						) : (
+							<div
+								style={{
+									width: '90%',
+									padding: '20px',
+									height: '100%',
+									border: '1px solid white',
+									margin: '60px auto 20px auto',
+								}}
+							>
+								{contentInfo?.details}
+							</div>
+						)}
+					</>,
+					<>
+						{contentInfo?.details.length === 0 ? (
+							<div className={styles.tabpannel}>
+								강사 소개가 없습니다.
+							</div>
+						) : (
+							<div
+								style={{
+									width: '90%',
+									padding: '20px',
+									height: '100%',
+									border: '1px solid white',
+									margin: '60px auto 20px auto',
+								}}
+							>
+								{contentInfo?.tutorDetail}
+							</div>
+						)}
+					</>,
+					<>
+						{contentInfo?.details.length === 0 ? (
+							<div className={styles.tabpannel}>
+								커리큘럼이 없습니다.
+							</div>
+						) : (
+							<CurriculumInfo
+								userInfo={userInfo}
+								role={contentInfo?.role}
+								contentsId={contentInfo?.contentsId}
+								curriculumInfo={curriculumInfo}
+							/>
+						)}
+					</>,
+				]}
+			/>
+		</>
+	);
 };
 export default ContentTabs;
