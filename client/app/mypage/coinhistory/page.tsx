ef7c2b0349523c86c16a7c3ab3d432bf8bc21d26
podoco <103816228+podoco@users.noreply.github.com>
2022-12-05 21:58:15 +0900

    코인내역 로직 완성

--- client/app/mypage/coinhistory/page.tsx
+++ client/app/mypage/coinhistory/page.tsx
@@ -1,35 +1,32 @@
+import { cookies } from "next/headers";
 import React from "react";
 import BaseNavbar from "../../../components/BaseNavBar/BaseNavbar";
-import styles from "./coinhistory.module.css";
-const coinHistory = () => {
+import CoinHistorySection from "./CoinHistorySection";
+
+const getCoinCharge = async () => {
+  const token = cookies().get("accessToken")?.value;
+  try {
+    const res = await fetch(`https://pioneroroom.com/auth/coincharge`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const coinHistoryPage = async () => {
+  const data = await getCoinCharge();
   return (
     <>
       <BaseNavbar page={"back"} />
-      <section className={styles.coinHistoryWrapper}>
-        <div className={styles.title}>
-          <h1 className={styles.h1}>코인내역</h1>
-          <h3 className={styles.h3}>보유 코인 200원</h3>
-        </div>
-
-        <div className={styles.coinBoard}>
-          <div className={styles.boardtitle}>
-            <p> 내역</p>
-            <p> 금액</p>
-            <p> 날짜</p>
-          </div>
-
-          <div className={styles.board}>
-            <div className={styles.CoinContents}>
-              <span>코안충전</span>
-              <span>5000원</span>
-              <span> 2022년 10.12</span>
-            </div>
-            <div className={styles.line} />
-          </div>
-        </div>
-      </section>
+      <CoinHistorySection data={data} />
     </>
   );
 };
 
-export default coinHistory;
+export default coinHistoryPage;
