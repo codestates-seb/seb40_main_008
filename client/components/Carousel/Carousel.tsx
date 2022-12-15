2b263e74a10025dbf773865d0ef8c2647fc05b60
akdlsz21 <akdlsz21@gmail.com>
2022-12-16 00:08:15 +0900

    fix: fixed merge error

--- client/components/Carousel/Carousel.tsx
+++ client/components/Carousel/Carousel.tsx
@@ -10,7 +10,13 @@ const Carousel = ({ carousel }: CarouselProp) => {
 	return (
 		<>
 			{carousel.map((e: CarouselInfo) => (
-				<Image alt="carousel image" src={e.src} key={e.id} width={300} height={300} />
+				<Image
+					alt="carousel image"
+					src={e.imageUrl}
+					key={e.carouselId}
+					width={300}
+					height={300}
+				/>
 			))}
 		</>
 	);
